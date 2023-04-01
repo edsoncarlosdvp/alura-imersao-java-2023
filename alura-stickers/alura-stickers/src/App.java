b39f88983b02cc3d3c193fdd3e7f8eaf7cd66bc7
Edson Carlos <edsoncarlos@ymail.com>
2023-04-01 19:16:46 -0300

    #02 fix: Aprimoramentos e correções

--- alura-stickers/alura-stickers/src/App.java
+++ alura-stickers/alura-stickers/src/App.java
@@ -1,4 +1,5 @@
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
@@ -7,7 +8,8 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_4l2nmlmg";
+        // String url = "https://imdb-api.com/en/API/Top250Movies/k_4l2nmlmg";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 
         var adress = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -18,10 +20,19 @@ public class App {
         var parse = new JsonParser();
         List<Map<String, String>> listMovies = parse.parse(body);
         
-        for (Map<String, String> movies : listMovies) {
-            System.out.println(movies.get("title"));
-            System.out.println(movies.get("image"));
-            System.out.println(movies.get("imDbRating"));
+        for (int i = 0; i < 10; i++) {
+            Map<String, String> movie = listMovies.get(i);
+
+            String urlImage = movie.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
+            String title = movie.get("title");
+
+            var inputStream = new URL(urlImage).openStream();
+            String fileName = title + ".png";
+
+            var generator = new StickersGeneration();
+            generator.Create(inputStream, fileName);
+
+            System.out.println(title);
             System.out.println();
         }
     }
